0ec7c9eb2e330b0d310debcb321a4c24147f35d8
Alain Filion <filion_alain@hotmail.com>
2021-01-07 23:26:56 -0500

    Mise en place d'une version rudimentaire du site

--- liste-repertoire-frontend/src/App.js
+++ liste-repertoire-frontend/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+import PageAccueil from './pages/PageAccueil';
+import PageRepertoire from './pages/PageRepertoire';
+import PageAdmin from './pages/PageAdmin';
+import PageAjouter from './pages/PageAjouter';
+import PageModifier from './pages/PageModifier';
+import PageSupprimer from './pages/PageSupprimer';
+import Page404 from './pages/Page404';
+import BarreNavigation from './composants/BarreNavigation';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <BarreNavigation />
+        <Switch>
+          <Route path="/" component={PageAccueil} exact />
+          <Route path="/repertoire" component={PageRepertoire} />
+          <Route path="/admin" component={PageAdmin} />
+          <Route path="/ajouter" component={PageAjouter} />
+          <Route path="/modifier/:id" component={PageModifier} />
+          <Route path="/supprimer/:id" component={PageSupprimer} />
+          <Route component={Page404} />
+        </Switch>        
+      </Container>
+    </Router>
   );
 }
 
---
+++ liste-repertoire-frontend/src/pages/PageAdmin.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import ListePieces from '../composants/ListePieces';
+
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
+function PageAdmin() {
+    return (
+        <>
+            <h1>Page administrateur</h1>
+
+            <Link to="/ajouter">
+                <Button>Ajouter une nouvelle pièce</Button>    
+            </Link>
+            
+            <h2>Liste du répertoire</h2>
+            <ListePieces />
+        </>
+    );
+}
+
+export default PageAdmin;
